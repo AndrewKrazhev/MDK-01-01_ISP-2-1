5da7c7e75409eb96ef7479691484a097418c7035
Andrew Kryazhev <accumhelp@ya.ru>
2023-03-21 10:57:57 +0300

    21 марта, работа с комбобокс

--- MyBD_prj/InputForm.cs
+++ MyBD_prj/InputForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace MyBD_prj
 {
@@ -15,6 +16,27 @@ namespace MyBD_prj
         public InputForm()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add(textBox1.Text); 
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label1.Text = comboBox1.SelectedItem.ToString();
+            label2.Text = comboBox1.Items[2].ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add(comboBox1.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Remove(comboBox1.SelectedItem);
         }
     }
 }
